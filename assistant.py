f0c87f04acc582548311fde21d9995b5fc37e215
Debangsh Bose <59859396+debangshb@users.noreply.github.com>
2021-07-12 11:45:00 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -16,10 +16,11 @@ import time
 from geopy.geocoders import Nominatim
 
 
-engiene=pyttsx3.init('sapi5')
-voices= engiene.getProperty('voices')
+engiene = pyttsx3.init('sapi5')
+voices = engiene.getProperty('voices')
 print(voices[1].id)
 engiene.setProperty('voice', voices[2].id)
+
 
 def speak(audio):
     engiene.say(audio)
@@ -28,13 +29,12 @@ def speak(audio):
 
 
 def takeCommand():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold=2
-        r.energy_threshold=700
-        audio=r.listen(source, timeout=1, phrase_time_limit=5)
-
+        r.pause_threshold = 2
+        r.energy_threshold = 700
+        audio = r.listen(source, timeout=1, phrase_time_limit=5)
 
         try:
             print("Recognizing...")
@@ -48,29 +48,28 @@ def takeCommand():
 
 
 def wishme():
-    hour=int(datetime.datetime.now().hour)
+    hour = int(datetime.datetime.now().hour)
     tt = time.strftime("%I:%M %p")
 
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         watch = datetime.date.today()
         speak("Good Morning")
-        speak(f"I am chelsea Sir. It is {watch} and the time is {tt} . Please tell me how can i help you?")
+        speak(
+            f"I am chelsea Sir. It is {watch} and the time is {tt} . Please tell me how can i help you?")
 
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         watch = datetime.date.today()
-        
-        speak("Good Afternoon")
-        speak(f"I am chelsea Sir. It is {watch} and the time is {tt} . Please tell me how can i help you?")
 
+        speak("Good Afternoon")
+        speak(
+            f"I am chelsea Sir. It is {watch} and the time is {tt} . Please tell me how can i help you?")
 
     else:
         watch = datetime.date.today()
-        
+
         speak("Good Evening")
-        speak(f"I am chelsea Sir. It is {watch} and the time is {tt} . Please tell me how can i help you?")
-
-
-    
+        speak(
+            f"I am chelsea Sir. It is {watch} and the time is {tt} . Please tell me how can i help you?")
 
 
 def sendEmail(to, content):
@@ -79,7 +78,7 @@ def sendEmail(to, content):
     server.starttls()
     speak("sir put your email id and password")
     email = input()
-    password=input()
+    password = input()
     server.login(f'{email}', f'{password}')
     server.sendmail(email, to, content)
     server.close()
@@ -91,19 +90,21 @@ def news():
     main_page = requests.get(main_url).json()
     articles = main_page['articles']
     head = []
-    day = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"]
+    day = ["first", "second", "third", "fourth", "fifth",
+           "sixth", "seventh", "eighth", "ninth", "tenth"]
     for ar in articles:
         head.append(ar["title"])
-    for i in range (len(day)):
+    for i in range(len(day)):
         speak(f"today's {day[i]} news is: {head[i]}")
 
+
 if __name__ == "__main__":
-    clear = lambda: os.system('cls')
+    def clear(): return os.system('cls')
     clear()
     wishme()
     while True:
 
-        query=takeCommand().lower()
+        query = takeCommand().lower()
 
         if 'open notepad' in query:
             ntpath = 'C:\\Windows\\system32\\notepad.exe'
@@ -129,9 +130,8 @@ if __name__ == "__main__":
             speak("Okay sir, command prompt")
             os.system("taskkill /f /im cmd.exe")
 
-        
         # elif 'open camera' in query:
-        #     cap = 
+        #     cap =
         # elif "camera" in query or "take a photo" in query:
         #     ec.capture(0, "Jarvis Camera ", "img.jpg")
 
@@ -155,7 +155,6 @@ if __name__ == "__main__":
             speak("Okay sir, closing google")
             os.system("taskkill /f /im chrome.exe")
 
-        
         elif 'ip address' in query:
             ip = requests.get('https://api.ipify.org').text
             speak(f"your ip address is {ip}")
@@ -163,8 +162,8 @@ if __name__ == "__main__":
 
         elif 'wikipedia' in query:
             speak("searching wikipedia...")
-            query=query.replace("wikipedia", "")
-            results=wikipedia.summary(query, sentences=3)
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=3)
             speak("According to wikipedia")
             speak(results)
             print(results)
@@ -175,7 +174,6 @@ if __name__ == "__main__":
         elif 'close youtube' in query:
             speak("Okay sir, closing youtube")
             os.system("taskkill /f /im iexplore.exe")
-            
 
         elif 'open instagram' in query:
             webbrowser.open("www.instagram.com")
@@ -191,7 +189,6 @@ if __name__ == "__main__":
             speak("Okay sir, closing facebook")
             os.system("taskkill /f /im chrome.exe")
 
-
         elif 'open geeksforgeeks' in query:
             webbrowser.open("www.geeksforgeeks.org")
 
@@ -199,20 +196,19 @@ if __name__ == "__main__":
             speak("Okay sir, closing geeksforgeeks")
             os.system("taskkill /f /im chrome.exe")
 
-
         elif 'search something' in query:
             speak("What do you want to search")
-            sch= takeCommand().lower()
+            sch = takeCommand().lower()
             webbrowser.open(f"{sch}")
 
         elif 'send a message' in query:
-          speak("what should i say sir")
-          msg=takeCommand()
-          speak("to whom should i send")
-          to=takeCommand()
-          hr= int(datetime.datetime.now().hour)
-          min= int(datetime.datetime.now().minute)  
-          kit.sendwhatmsg(f"+91{to}",f"{msg}" ,20,13)
+            speak("what should i say sir")
+            msg = takeCommand()
+            speak("to whom should i send")
+            to = takeCommand()
+            hr = int(datetime.datetime.now().hour)
+            min = int(datetime.datetime.now().minute)
+            kit.sendwhatmsg(f"+91{to}", f"{msg}", 20, 13)
 
         elif 'listen a song on youtube' in query:
             speak("what do you want to listen sir")
@@ -223,7 +219,7 @@ if __name__ == "__main__":
             try:
                 speak("What should i say?")
                 content = takeCommand()
-                speak("Sir please write the email id To whom should i send")    
+                speak("Sir please write the email id To whom should i send")
                 to = input()
                 sendEmail(to, content)
                 speak("Email has been sent Sir")
@@ -233,17 +229,17 @@ if __name__ == "__main__":
                 speak("Sorry Sir, I am unable to send this email")
 
         elif 'elif set alarm' in query:
-            nm= int(datetime.datetime.now().hour)
+            nm = int(datetime.datetime.now().hour)
             speak("When should i set the alarm Sir?")
-            alrm= takeCommand()
-            if nm==alrm:
+            alrm = takeCommand()
+            if nm == alrm:
                 music_dir = 'D:\\e backup\\MANER MANUS'
-                songs=os.listdir(music_dir)
+                songs = os.listdir(music_dir)
                 os.startfile(os.path.join(music_dir, songs[0]))
 
         elif 'you can sleep' in query:
-         speak("Thanks for using me sir, have a good day")       
-         sys.exit()
+            speak("Thanks for using me sir, have a good day")
+            sys.exit()
 
         elif 'tell me a joke' in query:
             joke = pyjokes.get_joke()
@@ -256,7 +252,7 @@ if __name__ == "__main__":
             os.system("shutdown /r /t S")
 
         elif 'sleep the system' in query:
-            os.system("Rundll32.exe powrprof.dll,SetSuspendState Sleep 0,1,0")  
+            os.system("Rundll32.exe powrprof.dll,SetSuspendState Sleep 0,1,0")
 
         elif 'switch the window' in query:
             pyautogui.keyDown('alt')
@@ -278,9 +274,11 @@ if __name__ == "__main__":
                 geo_data = geo_requests.json()
                 city = geo_data['city']
                 country = geo_data['country']
-                speak(f"Sir I am not sure but I think we are in {city} city of country {country}")
+                speak(
+                    f"Sir I am not sure but I think we are in {city} city of country {country}")
             except Exception as e:
-                speak("Sorry sir due to network issue i am not able to find where we are")
+                speak(
+                    "Sorry sir due to network issue i am not able to find where we are")
                 pass
 
         # elif 'profile picture' or 'instagram profile' in query:
@@ -294,7 +292,6 @@ if __name__ == "__main__":
         #     if 'yes' in condition:
         #         mod = insta
 
-
         elif 'take screenshot' in query or 'take a screenshot' in query:
             speak("Please tell me the name for this screenshot file")
             name = takeCommand().lower()
@@ -303,6 +300,17 @@ if __name__ == "__main__":
             img.save(f"{name}.png")
             speak("I am done sir. The screenshot is saved in our main folder")
 
+        elif 'how are you' in query:
+            speak("I am fine, Thank you")
+            speak("How are you, Sir")
+
+        elif 'I am fine' in query or " I am doing good" in query or 'I am doing great' in query or ' I am well' in query:
+            speak("It's good to know that your fine")
+
+        elif 'not fine' in query or 'not doing fine' in query or 'not doing well' in query:
+            speak("why Sir?")
+            takeCommand().lower
+            speak("I hope you will get through it")
+
         else:
             pass
-            
